15a7a4344cb270f6873f5fe8611ddd13c54deb98
Ilia <isinetov@mail.ru>
2024-05-18 18:09:05 +0400

    Задание 15.2.8
    Напишите программу с бесконечным циклом, как в предыдущем юните, которая будет:
    
    ожидать ввода числа с клавиатуры (используйте Console.ReadLine());
    добавлять число в список, хранящийся в памяти;
    выводить после добавления следующую информацию: количество чисел в списке, сумму всех чисел списка, наибольшее и наименьшее числа, а также их среднее значение.

---
+++ Task2.8/Task2.8.cs
@@ -0,0 +1,27 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        int num;
+        int numsCount;
+        int numsSum;
+        int numMax;
+        int numMin;
+        double numAverage;
+        var numsList = new List<int>();
+
+        while (true) 
+        {
+            Console.Write("Введите целое число с клавиатуры: ");
+            num = int.Parse(Console.ReadLine());
+
+            numsList.Add(num);
+
+            Console.WriteLine($"Кол-во чисел в списке: {numsList.Count}");
+            Console.WriteLine($"Сумма чисел в списке: {numsList.Sum()}");
+            Console.WriteLine($"Максимальное число в списке: {numsList.Max()}");
+            Console.WriteLine($"Минимальное число в списке: {numsList.Min()}");
+            Console.WriteLine($"Среднее число в списке: {numsList.Average()}");
+        }
+    }
+}
