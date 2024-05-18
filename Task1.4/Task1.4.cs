f1c9d62817d5f791d0914d86252b58547cefe9e5
Ilia <isinetov@mail.ru>
2024-05-18 16:46:57 +0400

    Задание 15.1.4 Напишите метод для поиска общих букв в двух словах.

---
+++ Task1.4/Task1.4.cs
@@ -0,0 +1,15 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        string word1 = "нолзщфл";
+        string word2 = "пошцфоз";
+
+        var commonLetters = word1.Intersect(word2);
+
+        foreach (var letter in commonLetters) 
+        {
+            Console.Write(letter);
+        }
+    }
+}
