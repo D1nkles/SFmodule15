9202f12659614d85f15ccd8fb7e59ff6abfddaf9
Ilia <isinetov@mail.ru>
2024-05-18 17:01:57 +0400

    Задание 15.1.6 Напишите программу, которая будет принимать на вход строку текста с консоли (конструкция Console.Readline()) и выводить в ответ список содержащихся в строке уникальных символов без пробелов и следующих знаков препинания: , . ; :  ? !.

---
+++ Task1.6/Task1.6.cs
@@ -0,0 +1,18 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        Console.WriteLine("Введите произвольную строку символов...");
+        string text = Console.ReadLine();
+
+        List<char> punctuation = new List<char>{' ', ',' , '.', '!', ';', ':', '?'};
+
+        var uniqueLetters = text.Distinct();
+        var noPunctuation = uniqueLetters.Except(punctuation);
+
+        foreach (var letter in noPunctuation) 
+        {
+            Console.Write(letter + " ");
+        }
+    }
+}
