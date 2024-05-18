c6eeb4d66921b6e274803a14181190ccb2569a96
Ilia <isinetov@mail.ru>
2024-05-18 17:36:46 +0400

    Задание 15.2.1
    Факториал натурального числа n — это произведение всех натуральных целых чисел от 1 до n включительно
    
    Допишите функцию, используя знания LINQ, полученные в этом юните.

---
+++ Task2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        long num = 10;
+
+        List<int> factorialNums = new List<int>();
+        for (int i = 1; i <= num ; i++) 
+        {
+            factorialNums.Add(i);
+        }
+
+        long factorial = factorialNums.Aggregate((x, y) => x * y);
+        Console.WriteLine(factorial);
+    }
+}
