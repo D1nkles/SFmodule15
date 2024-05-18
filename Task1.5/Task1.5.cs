2f6b3fec593bd4ca0771e78b86ba724fe4b80e6b
Ilia <isinetov@mail.ru>
2024-05-18 16:49:31 +0400

    Задание 15.1.5 Напишите недостающий код так, чтобы на выходе мы получили список всех IT-компаний без повторений.

---
+++ Task1.5/Task1.5.cs
@@ -0,0 +1,22 @@
+﻿class Program 
+{
+    static void Main(string[] args) 
+    {
+        var softwareManufacturers = new List<string>()
+        {
+            "Microsoft", "Apple", "Oracle"
+        };
+
+        var hardwareManufacturers = new List<string>()
+        {
+            "Apple", "Samsung", "Intel"
+        };
+
+        var itCompanies = softwareManufacturers.Union(hardwareManufacturers);
+        
+        foreach (var it in itCompanies) 
+        {
+            Console.Write(it + " ");
+        }
+    }
+}
